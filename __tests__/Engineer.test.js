faacc41329d65d335104afc0b0fce7eea8622386
Tim Weyel <tweyel@gmail.com>
2021-04-08 11:14:11 -0700

    Engineer object added. all tests passing

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,20 @@
 const Engineer = require('../lib/Engineer.js');
+
+test('can set a github username', () => {
+  const githubUser = "GitHubUsername";
+  const employee = new Engineer("Tim", 12345, "email@email.com", githubUser);
+  expect(employee.githubUser).toBe(githubUser);
+});
+
+test('can getGithub()', () => {
+  const githubUser = "GitHubUserName";
+  const employee = new Engineer("Tim", 12345, "email@email.com", githubUser);
+  expect(employee.getGithub()).toBe(githubUser);
+})
+
+
+test("getRole() returns Engineer", () => {
+  const role = "Engineer";
+  const employee = new Engineer("Tim", 12345, "email@email.com", "GitHubUserName");
+  expect(employee.getRole()).toBe(role);
+});
