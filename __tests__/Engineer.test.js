b998c989a524a92fcff216d1005fdd542f4114b4
Tim Weyel <tweyel@gmail.com>
2021-04-08 10:40:59 -0700

    manager object added. tests passing

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,1 @@
 const Engineer = require('../lib/Engineer.js');
-
-test('creates an engineer object', () => {
-  const engineer = new Engineer();
-
-  expect(typeof(engineer)).toBe('object');
-});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,20 @@
 const Manager = require('../lib/Manager.js');
+const Employee = require('../lib/Employee.js');
 
-test('creates an manager object', () => {
-  const manager = new Manager();
+test('can set an office number', () => {
+  const officeNum = 123;
+  const employee = new Manager("Tim", 12345, "email@email.com", officeNum);
+  expect(employee.officeNumber).toBe(officeNum);
+});
 
-  expect(typeof(manager)).toBe('object');
+test('can getOffice()', () => {
+  const officeNum = 123;
+  const employee = new Manager("Tim", 12345, "email@email.com", officeNum);
+  expect(employee.getOffice()).toBe(officeNum);
+});
+
+test("getRole() returns Manager", () => {
+  const role = "Manager";
+  const employee = new Manager("Tim", 12345, "email@email.com", 123);
+  expect(employee.getRole()).toBe(role);
 });
