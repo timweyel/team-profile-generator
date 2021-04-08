7c9f2281af7e75c58870af6df23dd52645b5f3f0
Tim Weyel <tweyel@gmail.com>
2021-04-08 11:37:13 -0700

    added Intern object. all tests pass

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,20 @@
 const Intern = require('../lib/Intern.js');
 
-test('creates an intern object', () => {
-  const intern = new Intern();
+test('can set school', () => {
+  const school = "Cal Poly SLO";
+  const employee = new Intern("Tim", 12345, "email@email.com", school);
+  expect(employee.school).toBe(school);
+});
 
-  expect(typeof(intern)).toBe('object');
+test('can getSchool()', () => {
+  const school = "Cal Poly SLO";
+  const employee = new Intern("Tim", 12345, "email@email.com", school);
+  expect(employee.getSchool()).toBe(school);
+})
+
+
+test("getRole() returns Intern", () => {
+  const role = "Intern";
+  const employee = new Intern("Tim", 12345, "email@email.com", "Cal Poly SLO");
+  expect(employee.getRole()).toBe(role);
 });
