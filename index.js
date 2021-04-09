bf84ee0fa9f6a77aa80b964fde91f90da5d91928
Tim Weyel <tweyel@gmail.com>
2021-04-09 13:40:28 -0700

    added addManager

--- index.js
+++ index.js
@@ -2,68 +2,60 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const template = require('./src/pageTemplate');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-// initialize an inquirer prompt
 // create variables to hold questions for Manager, Engineer, Interns (each has their own variable)
   // Manager: ask for manager's name, id, email, office number
   // Engineer: ask for the engineer's name, email, and github username
   // Intern: ask for intern's name, email, and school
 
-//create a function to prompt questions for manager, engineer, and intern (each has their own function to manager prompts)
-  //within each of these functions, add the answers to a responses object
-  
-// writeFile team members to html -> generatePage()
-  //lookup from last challenge how to do a write and copy because of css
-// use bootstrap cards
-// 
-
-
-
 const managerQuestions = [
   //TODO: add validation to inputs
-    {
-      type: "input",
-      name: "name",
-      message: "Please enter the manager's name",
-    },
-    {
-      type: "number",
-      name: "id",
-      message: "What is the manager's employee ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the manager's email address?",
-      //TODO: cleanup this UX - how to clear input after submission?
-      // credit to: https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8
-      default: () => {},
-      validate: function (email) {
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter the manager's name",
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "What is the manager's employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the manager's email address?",
+    //TODO: cleanup this UX - how to clear input after submission?
+    // credit to: https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8
+    default: () => {},
+    validate: function (email) {
 
-          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 
-          if (valid) {
-            // console.log("Great job");
-              return true;
-          } else {
-              console.log(".  Please enter a valid email")
-              return false;
-          }
-        // end credit
+        if (valid) {
+          // console.log("Great job");
+            return true;
+        } else {
+            console.log(".  Please enter a valid email")
+            return false;
         }
-    },
-    {
-      type: "number",
-      name: "officeNumber",
-      message: "What is the manager's office number?"
-    },
-    {
-      type: "list",
-      name: "addEmployee",
-      message: "Would you like to add more team members?",
-      choices: ["Yes, add an Engineer.", "Yes, add an Intern", "No, not right now."]
-    },
-  ];
+      // end credit
+      }
+  },
+  {
+    type: "number",
+    name: "officeNumber",
+    message: "What is the manager's office number?"
+  },
+  {
+    type: "list",
+    name: "addEmployee",
+    message: "Would you like to add more team members?",
+    choices: ["Yes, add an Engineer.", "Yes, add an Intern", "No, not right now."]
+  },
+];
 
 const engineerQuestions = [
     // Engineer: ask for the engineer's name, email, and github username
@@ -121,21 +113,54 @@ const internQuestions = [
   },
   {
     type: "list",
-    name: "addEmployee",
+    name: "addTeammate",
     message: "Would you like to add more team members?",
     choices: ["Yes, add an Engineer.", "Yes, add an Intern", "No, not right now."]
   }
 ];
 
+//create a function to prompt questions for manager, engineer, and intern (each has their own function to manager prompts)
+  //within each of these functions, add the answers to a responses object
+  
+
    //TODO - add function to addManager();
+const addManager = () => {
+  inquirer
+  .prompt(managerQuestions)
+  .then(responses => {
+    const manager = new Manager(responses.name, responses.id, responses.email, responses.officeNumber);
+    if(!manager.teamMembers){
+      manager.teamMembers = [];
+    }
+    if(responses.addTeammate === 'Yes, add an Engineer.') {
+      addEngineer(manager);
+    }
+    if(responses.addTeammate === 'Yes, add an Intern.') {
+      addIntern(manager);
+    }
+    else if(responses.addTeammate === 'No, not right now.') {
+      createTeamProfile(manager);
+    }
+    console.log('responses from addManager', responses);
+  });
+};
 
    //TODO - add function to addEngineer();
+const addEngineer = manager => {
+  // inquirer
+  // .prompt(engineerQuestions)
+  // .then(responses)
+
+}
 
    //TODO - add function to addIntern();
 
-   //TODO - add function to write to html called createTeamProfile()
+   //TODO - add function to writeFile to html called createTeamProfile()
 
-
+// writeFile team members to html -> generatePage()
+  //lookup from last challenge how to do a write and copy because of css
+// use bootstrap cards
+// 
 
 //function to init app
 const init = () => {
